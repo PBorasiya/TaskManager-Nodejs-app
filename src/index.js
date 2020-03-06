bddcc0c4f7b00baba19045fa8ba5235ba4663134
PBorasiya <parixit.borasia@gmail.com>
2020-03-05 18:01:53 -0800

    Created GET call to read a single tasks by id

--- src/index.js
+++ src/index.js
@@ -56,6 +56,19 @@ app.get('/tasks', (req,res) => {
     })
 })
 
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.listen(port,()=>{
     console.log('Serves is up on port ' + port)
 })

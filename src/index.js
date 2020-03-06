c2727e6e660602cf10021f07685bdac97d28009d
PBorasiya <parixit.borasia@gmail.com>
2020-03-05 17:52:19 -0800

    Created GET call to retrieve single user by ID

--- src/index.js
+++ src/index.js
@@ -25,6 +25,20 @@ app.get('/users',(req,res) => {
     })
 })
 
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    
+    User.findById({_id}).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+    
+})
+
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body)
     task.save().then(()=>{

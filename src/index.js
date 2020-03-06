babec16b51d4aff4adb6ce931c4f503574f94f69
PBorasiya <parixit.borasia@gmail.com>
2020-03-05 17:39:57 -0800

    Created GET call to retrieve all users

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ app.post('/users',(req,res)=>{
     })
 })
 
+app.get('/users',(req,res) => {
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body)
     task.save().then(()=>{

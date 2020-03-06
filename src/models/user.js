30dea27864c260e6ca850076c62d88f8755f0334
PBorasiya <parixit.borasia@gmail.com>
2020-03-05 17:03:26 -0800

    init commint

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User',{ 
+    name : {
+        type :String,
+        trim : true
+    },
+    email: {
+        type : String,
+        required : true,
+        trim : true,
+        lowercase : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is not valid');
+                
+            }
+        }
+    },
+    password: {
+        type : String,
+        required : true,
+        trim : true,
+        minlength : 7,
+        validate(value){
+            
+            if (value.toLowerCase().includes('password')){
+                throw new Error('Please follow password word rules')
+            }
+        }
+    },
+    age : {
+        type : Number,
+        default : 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age cannot be negative')
+            }
+        }
+    }
+})
+
+
+module.exports = User

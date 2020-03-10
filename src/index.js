1a912392d73327ab1b71749b43448879f00bb456
PBorasiya <parixit.borasia@gmail.com>
2020-03-09 23:02:38 -0700

    Modified index.js to use Async/await

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users',(req,res)=>{
+// app.post('/users',(req,res)=>{
+//     const user = new User(req.body)
+//     user.save().then(()=>{
+//         res.status(201).send(user)
+//     }).catch((error)=>{
+//         res.status(400).send(error)
+//     })
+// })
+
+app.post('/users', async (req,res) => {
     const user = new User(req.body)
-    user.save().then(()=>{
+
+    try{
+
+        await user.save()
         res.status(201).send(user)
-    }).catch((error)=>{
-        res.status(400).send(error)
-    })
+
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
 
 app.get('/users',(req,res) => {

9327c76786f248a03bdc31b4197518b0c519a6f0
PBorasiya <parixit.borasia@gmail.com>
2020-03-10 17:39:08 -0700

    Reformatted code in index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-// app.post('/users',(req,res)=>{
-//     const user = new User(req.body)
-//     user.save().then(()=>{
-//         res.status(201).send(user)
-//     }).catch((error)=>{
-//         res.status(400).send(error)
-//     })
-// })
-
 app.post('/users', async (req,res) => {
     const user = new User(req.body)
 
@@ -30,14 +21,6 @@ app.post('/users', async (req,res) => {
     }
 })
 
-// app.get('/users',(req,res) => {
-//     User.find({}).then((users)=>{
-//         res.send(users)
-//     }).catch((e)=>{
-//         res.status(500).send()
-//     })
-// })
-
 app.get('/users', async (req,res) => {
     try{
         const users = await User.find({})
@@ -46,20 +29,6 @@ app.get('/users', async (req,res) => {
         res.status(404).send(e)
     }
 })
-
-// app.get('/users/:id', (req, res) => {
-//     const _id = req.params.id
-    
-//     User.findById({_id}).then((user) => {
-//         if(!user){
-//             return res.status(404).send()
-//         }
-//         res.send(user)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-    
-// })
 
 app.get('/users/:id', async (req,res) => {
     const _id = req.params.id;
@@ -75,16 +44,6 @@ app.get('/users/:id', async (req,res) => {
     }
 })
 
-
-// app.post('/tasks',(req,res)=>{
-//     const task = new Task(req.body)
-//     task.save().then(()=>{
-//         res.status(201).send(task)
-//     }).catch((e)=>{
-//         res.status(400).send(e)
-//     })
-// })
-
 app.post('/tasks', async ( req, res ) => {
     const task = new Task(req.body)
 
@@ -95,14 +54,6 @@ app.post('/tasks', async ( req, res ) => {
         res.status(404).send(e)
     }
 })
-
-// app.get('/tasks', (req,res) => {
-//     Task.find({}).then((tasks) => {
-//         res.send(tasks)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
 
 app.get('/tasks', async (req, res) => {
     try{
@@ -117,19 +68,6 @@ app.get('/tasks', async (req, res) => {
         res.status(500).send(e)
     }
 })
-
-// app.get('/tasks/:id', (req,res) => {
-//     const _id = req.params.id
-
-//     Task.findById(_id).then((task) => {
-//         if(!task){
-//             return res.status(404).send()
-//         }
-//         res.send(task)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
 
 app.get('/tasks/:id', async (req,res) => {
     const _id = req.params.id
@@ -186,7 +124,67 @@ app.patch('/tasks/:id' , async ( req, res ) => {
     }
 })
 
-
 app.listen(port,()=>{
     console.log('Serves is up on port ' + port)
 })
+
+// CODE WITHOUD ASYNC/AWAIT ONLY WORKS WITH PROMISE CHAINING. ABOVE CODE IS BETTER IN TERMS OF READABILITY
+// app.post('/users',(req,res)=>{
+//     const user = new User(req.body)
+//     user.save().then(()=>{
+//         res.status(201).send(user)
+//     }).catch((error)=>{
+//         res.status(400).send(error)
+//     })
+// })
+// app.get('/users',(req,res) => {
+//     User.find({}).then((users)=>{
+//         res.send(users)
+//     }).catch((e)=>{
+//         res.status(500).send()
+//     })
+// })
+// app.get('/users/:id', (req, res) => {
+//     const _id = req.params.id
+    
+//     User.findById({_id}).then((user) => {
+//         if(!user){
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     }).catch((e) => {
+//         res.status(500).send()
+//     })
+    
+// })
+
+
+// app.post('/tasks',(req,res)=>{
+//     const task = new Task(req.body)
+//     task.save().then(()=>{
+//         res.status(201).send(task)
+//     }).catch((e)=>{
+//         res.status(400).send(e)
+//     })
+// })
+// app.get('/tasks', (req,res) => {
+//     Task.find({}).then((tasks) => {
+//         res.send(tasks)
+//     }).catch((e) => {
+//         res.status(500).send()
+//     })
+// })
+// app.get('/tasks/:id', (req,res) => {
+//     const _id = req.params.id
+
+//     Task.findById(_id).then((task) => {
+//         if(!task){
+//             return res.status(404).send()
+//         }
+//         res.send(task)
+//     }).catch((e) => {
+//         res.status(500).send()
+//     })
+// })
+
+

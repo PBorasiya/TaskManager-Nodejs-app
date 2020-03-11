570f93febf7c70869a3aa739d6aae62fea1f3e02
PBorasiya <parixit.borasia@gmail.com>
2020-03-10 17:21:43 -0700

    Modified index.js to make patch method able to handle invalid/bad request making sure which updates are allowed

--- src/index.js
+++ src/index.js
@@ -146,6 +146,13 @@ app.get('/tasks/:id', async (req,res) => {
 })
 
 app.patch('/users/:id' , async (req, res) =>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email' ,'password' , 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({ error : 'Invalid update! property does not exist'})
+    }
     try{
         const user = await User.findByIdAndUpdate(req.params.id, req.body , { new : true, runValidators : true})
         if(!user){

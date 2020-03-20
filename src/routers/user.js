93b6534a838f5138591c9f2e1e5abd0b71fc852f
PBorasiya <parixit.borasia@gmail.com>
2020-03-19 19:24:32 -0700

    updated login endpoint to hide personal data such as password

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,7 @@ router.post('/users/login', async (req, res) =>{
     try{
         const user = await User.loginUser(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({user, token})
+        res.send({user : user.getPublicProfile(), token})
     }catch(e){
         res.status(400).send(e)
     }

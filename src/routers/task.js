7110d544f0d5ba4e70b993d4eb21869a65fd432d
PBorasiya <parixit.borasia@gmail.com>
2020-03-20 00:19:39 -0700

    updated GET/tasks endpoint to show all tasks for logged in user

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,25 +17,23 @@ router.post('/tasks', auth , async ( req, res ) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth ,async (req, res) => {
     try{
-
-        const tasks = await Task.find({})
-        if(!tasks){
-            return res.status(404).send()
-        }
-        res.status(200).send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.status(200).send(req.user.tasks)
         
     }catch(e){
         res.status(500).send(e)
     }
 })
 
-router.get('/tasks/:id', async (req,res) => {
+router.get('/tasks/:id', auth, async (req,res) => {
     const _id = req.params.id
 
     try{
-        const task = await Task.findById({_id})
+        //gets the task created by loggen in user
+        const task = await Task.findOne({ _id , owner : req.user._id })
+
         if(!task){
             return res.status(404).send()
         }

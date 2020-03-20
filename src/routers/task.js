e4e6829cd0232731142f04ae274f8de287d5b16e
PBorasiya <parixit.borasia@gmail.com>
2020-03-20 16:37:34 -0700

    added code to filter tasks based on querystring completed true or false

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,9 +18,19 @@ router.post('/tasks', auth , async ( req, res ) => {
 })
 
 router.get('/tasks', auth ,async (req, res) => {
+    const match = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
     try{
         //binds and populates logged in users tasks virtual tasks property 
-        await req.user.populate('tasks').execPopulate()
+        //populate function can be customized to help set querystring attributes
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.status(200).send(req.user.tasks)
         
     }catch(e){

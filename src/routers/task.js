f9aaeb33e90e583b7863d5fa11c65ace0e9c1e2c
PBorasiya <parixit.borasia@gmail.com>
2020-03-20 00:56:04 -0700

    updated Patch endpoint for loggen-in user to enable update by id

--- src/routers/task.js
+++ src/routers/task.js
@@ -45,7 +45,7 @@ router.get('/tasks/:id', auth, async (req,res) => {
 })
 
 
-router.patch('/tasks/:id' , async ( req, res ) => {
+router.patch('/tasks/:id' , auth ,async ( req, res ) => {
     
     const updates = Object.keys(req.body)
     const allowedUpdates = ['discription' , 'completed']
@@ -56,18 +56,13 @@ router.patch('/tasks/:id' , async ( req, res ) => {
     }
     
     try{
-        //const task = await Task.findByIdAndUpdate(req.params.id , req.body , { new : true , runValidators : true})
-        
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach((update) => task[update] = req.body[update])
-
-        await task.save()
-
+        const task = await Task.findOne({ _id : req.params.id, owner : req.user._id })
         if(!task){
             res.status(404).send()
         }
-        res.status(200).send(task)
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        res.send(task)
     }catch(e){
         res.status(400).send(e)
     }

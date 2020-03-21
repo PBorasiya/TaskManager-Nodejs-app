875db64a9c99340211b2c7d89486cafb78c4f99d
PBorasiya <parixit.borasia@gmail.com>
2020-03-20 18:34:31 -0700

    created an endpoint for uploading files

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 const router = new express.Router()
 
 router.post('/users', async (req,res) => {
@@ -56,19 +57,6 @@ router.get('/users/me', auth ,async (req,res) => {
     res.send(req.user)
 })
 
-// router.get('/users/:id', async (req,res) => {
-//     const _id = req.params.id;
-
-//     try{
-//         const user = await User.findById({_id})
-//         if(!user){
-//             return res.status(404).send()
-//         }
-//         res.status(200).send(user)
-//     }catch(e){
-//         res.status(500).send(e)
-//     }
-// })
 
 router.patch('/users/me', auth , async (req, res) =>{
     const updates = Object.keys(req.body)
@@ -112,6 +100,14 @@ router.delete('/users/me', auth , async (req, res) =>{
     }catch(e){
         res.status(400).send(e)
     }
+})
+
+const avatar = new multer({
+    dest : 'avatar'
+})
+
+router.post('/users/me/avatar', avatar.single('avatar') ,async(req,res)=>{
+    res.send()
 })
 
 module.exports = router

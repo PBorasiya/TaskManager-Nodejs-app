d5c6424a4509d82413eb3a0300379d30df0674db
PBorasiya <parixit.borasia@gmail.com>
2020-03-23 18:05:40 -0700

    used sharp package to convert incoming images to png

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 
 router.post('/users', async (req,res) => {

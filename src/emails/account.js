3805cca958fde374b41d90a3794d8edfb835a6a9
PBorasiya <parixit.borasia@gmail.com>
2020-03-25 20:21:21 -0700

    installed sendgrid for sending emails and added emails/account.js file to implement that

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,12 @@
-const sendgridAPIkey = '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'
+const sgMail = require('@sendgrid/mail')
+
+//const sendgridAPIkey = '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'
+
+sgMail.setApiKey(sendgridAPIkey)
+
+sgMail.send({
+    to : 'borasiyapranav@gmail.com',
+    from : 'borasiyapranav@gamil.com',
+    subject : 'Democheck for Sendgrid',
+    text : 'Hello Pranav from developer side'
+})

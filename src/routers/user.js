19b0b8dca8a38edfb0b87d8a92fbd9937bcd3a8c
PBorasiya <parixit.borasia@gmail.com>
2020-03-27 17:24:25 -0700

    pluged sendWelcome email to create User method to use the method created in last commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const { sendWelcomeEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req,res) => {
@@ -11,6 +12,7 @@ router.post('/users', async (req,res) => {
     try{
 
         await user.save()
+        sendWelcomeEmail(user.email , user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token})
 

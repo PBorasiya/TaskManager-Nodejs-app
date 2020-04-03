5bcad81c25af359b0e101ccdccfc7e52cf9c211f
PBorasiya <parixit.borasia@gmail.com>
2020-04-02 17:14:52 -0700

    added testing for Authorizing the user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+const userOneId = new mongoose.Types.ObjectId()
 
 const userOne = {
+    _id : userOneId,
     name : 'UserOne',
     email : 'parixit.borasia@gmail.com',
-    password : 'pranav123'
+    password : 'pranav123',
+    tokens : [{
+        token : jwt.sign({_id : userOneId} , process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async ()=>{
@@ -33,4 +41,12 @@ test('Should not log in non existing user',  async() =>{
         email : userOne.email,
         password : userOne.password + 's'
     }).expect(400)
+})
+
+test('Should get profile for a user' , async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization' , `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
 })

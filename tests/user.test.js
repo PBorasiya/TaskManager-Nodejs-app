db78d4da9b31533b9c05cf27a6039631f330857e
PBorasiya <parixit.borasia@gmail.com>
2020-04-02 17:02:42 -0700

    added setup for test suites to delete all entries in testing database before testing and also adding dummy data for logging in testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name : 'UserOne',
+    email : 'parixit.borasia@gmail.com',
+    password : 'pranav123'
+}
+
+beforeEach(async ()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sign up a new user', async ()=>{
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should sign up a new user', async ()=>{
         email : 'borasiyapranav@gmail.com',
         password : 'pranav123'
     }).expect(201)
+})
+
+test('Should log in existing user', async()=>{
+    await request(app).post('/users/login').send({
+        email : userOne.email,
+        password : userOne.password
+    }).expect(200)
+})
+
+test('Should not log in non existing user',  async() =>{
+    await request(app).post('/users/login').send({
+        email : userOne.email,
+        password : userOne.password + 's'
+    }).expect(400)
 })

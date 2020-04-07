ddd1f159fdc75ae1610cca3dcb922e34bd747195
PBorasiya <parixit.borasia@gmail.com>
2020-04-06 17:57:27 -0700

    tweaked some testing codes to assert the response coming in

--- tests/user.test.js
+++ tests/user.test.js
@@ -22,18 +22,35 @@ beforeEach(async ()=>{
 })
 
 test('Should sign up a new user', async ()=>{
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name : 'Pranav',
         email : 'borasiyapranav@gmail.com',
         password : 'pranav123'
     }).expect(201)
+
+    //assertions about response
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    expect(response.body).toMatchObject({
+        user : {
+            name : 'Pranav',
+            email : 'borasiyapranav@gmail.com'
+        },
+        token : user.tokens[0].token
+    })
+    expect(user.password).not.toBe('pranav123')
 })
 
 test('Should log in existing user', async()=>{
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email : userOne.email,
         password : userOne.password
     }).expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
+
 })
 
 test('Should not log in non existing user',  async() =>{
@@ -64,6 +81,9 @@ test('Should delete account for Authenticated user', async() =>{
         .set('Authorization' , `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unAuthenticated user', async() =>{
